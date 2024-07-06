5001d3a07bbd7fc4cff131bc9ecfce7bfb02d6a4
Ivan Krishanik <krishanik.ivan@gmail.com>
2024-07-06 14:23:19 +0200

    Initial Commit

---
+++ crystal-pool/crystal_poolApp.swift
@@ -0,0 +1,17 @@
+//
+//  crystal_poolApp.swift
+//  crystal-pool
+//
+//  Created by Ivan Krishanik on 06.07.24.
+//
+
+import SwiftUI
+
+@main
+struct crystal_poolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
